05d264246877a103c11f1ebf8f5c7beb46e48bd7
Phil Davis <phil.davis@inf.org>
2016-09-05 10:20:57 +0930

    Initial stuff

---
+++ src/Triangle.java
@@ -0,0 +1,118 @@
+/* ------------------------------
+ * File: Triangle.java
+ * Name: Dan Fleck
+ * Assignment: N/A
+ * Lab Section: All of them 
+ * Creation Date: Nov 10, 2008
+ * 
+ * References:
+ * Comments:
+ * ------------------------------
+ */
+
+// package junitexample;
+
+/**
+ *
+ * @author dfleck
+ */
+public class Triangle {
+
+    // -------------------------
+    // Class Attributes 
+    // -------------------------
+    private String side1;
+    private String side2;
+    private String side3;
+
+    // -------------------------
+    // Constructors
+    // -------------------------
+    public Triangle() {
+        // Create a default triangle
+        this("0", "0", "0");
+    }
+    
+    public Triangle(String s1, String s2, String s3) {
+        this.side1 = s1;
+        this.side2 = s2;
+        this.side3 = s3;
+    }
+    
+    // -------------------------
+    // Methods
+    // -------------------------
+    public String determineTriangleType() {
+        
+        String type = "";
+        
+        // This code runs when the user pushes the button
+        int s1 = -1, s2=-1, s3 = -1, temp=-1;
+        String err = "";
+        try {
+            s1 = Integer.parseInt(side1);
+        } catch (NumberFormatException e) {
+           err += "Side 1 is not a number!\n";
+        }
+        try {
+            s2 = Integer.parseInt(side2);
+        } catch (NumberFormatException e) {
+           err += "Side 2 is not a number!\n";
+        }
+        try {
+            s3 = Integer.parseInt(side3);
+        } catch (NumberFormatException e) {
+           err += "Side 3 is not a number!\n";
+        }
+        
+        // Check that nothing is negative
+        if (s1 <= 0 || s2 <= 0 || s3 <= 0) {
+               err += "At least one length is less than 0!\n";            
+        }
+        
+        if (s1 > s3) {
+        	temp = s3;
+        	s3 = s1;
+        	s1 = temp;
+        }
+        
+        if (s2 > s3) {
+        	temp = s3;
+        	s3 = s2;
+        	s2 = temp;
+        }
+        
+        if (s1 > s2) {
+        	temp = s2;
+        	s2 = s1;
+        	s1 = temp;
+        }
+        
+        // Check for side length
+        if ((s1 + s2 <= s3)) {
+               err += "The lengths of the triangles do not form a valid triangle!\n";
+        }  
+        
+        if (s1 + s2 + s3 > 1000000) {
+            err += "I feel your triangle is too big\n";
+        } 
+        
+        if (err.length() > 0) {
+           type = err;   
+        } else {            
+        	if ((s1 == s2) && (s2 == s3)) {
+               type= "Equilateral";
+           } else if ((s1 == s2) || (s2 == s3)) {
+               type = "Isosceles";
+           } else if (((s1 * s1) + (s2 * s2) == (s3 * s3))) {
+               type = "Right Angled";
+           } else {
+               type = "Scalene";
+           }                 
+        }
+        
+        return type;        
+    }
+
+     
+}
---
+++ src/TriangleTest.java
@@ -0,0 +1,199 @@
+/*
+ * JUnit test for the Triangle class
+ * This class works inside Netbeans -- See JUnit documentation for more info
+ * http://www.junit.org/
+ */
+// package junitexample;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dfleck
+ */
+public class TriangleTest {
+    // Create a new Triangle
+    public TriangleTest() {
+    }
+
+    @BeforeClass
+    public static void initClass() throws Exception {
+        System.out.println("\nINIT CLASS RUNNING -- Nothing to do though");
+    }
+
+    @AfterClass
+    public static void destroyClass() throws Exception {
+        System.out.println("\nDESTROY CLASS RUNNING -- Nothing to do though");
+    }
+
+    @Before
+    public void initCode() {
+        System.out.println("\nINIT CODE IS RUNNING -- Nothing to do though");
+    }
+
+    @After
+    public void destroyCode() {
+        System.out.println("DESTROY CODE IS RUNNING -- Nothing to do though");
+    }
+
+    @Test
+    public void checkScaleneA() {
+        System.out.println("Checking Scalene A");
+
+        Triangle instance = new Triangle("30", "40", "60");
+        String expResult = "Scalene";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkScaleneB() {
+        System.out.println("Checking Scalene B");
+
+        Triangle instance = new Triangle("30", "60", "40");
+        String expResult = "Scalene";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkScaleneC() {
+        System.out.println("Checking Scalene C");
+
+        Triangle instance = new Triangle("60", "30", "40");
+        String expResult = "Scalene";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkEquilateral() {
+        System.out.println("Checking Equilateral");
+        Triangle instance = new Triangle("1", "1", "1");
+        String expResult = "Equilateral";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkIsocelesA() {
+        System.out.println("Checking Isoceles A");
+        Triangle instance = new Triangle("30", "30", "40");
+        String expResult = "Isosceles";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkIsocelesB() {
+        System.out.println("Checking Isoceles B");
+        Triangle instance = new Triangle("30", "40", "30");
+        String expResult = "Isosceles";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkIsocelesC() {
+        System.out.println("Checking Isoceles C");
+        Triangle instance = new Triangle("40", "30", "30");
+        String expResult = "Isosceles";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkIsocelesD() {
+        System.out.println("Checking Isoceles D");
+        Triangle instance = new Triangle("40", "40", "30");
+        String expResult = "Isosceles";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkIsocelesE() {
+        System.out.println("Checking Isoceles E");
+        Triangle instance = new Triangle("40", "30", "40");
+        String expResult = "Isosceles";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkIsocelesF() {
+        System.out.println("Checking Isoceles F");
+        Triangle instance = new Triangle("30", "40", "40");
+        String expResult = "Isosceles";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkRightAngledA() {
+        System.out.println("Checking Right Angled A");
+        Triangle instance = new Triangle("3", "4", "5");
+        String expResult = "Right Angled";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkRightAngledB() {
+        System.out.println("Checking Right Angled B");
+        Triangle instance = new Triangle("3", "5", "4");
+        String expResult = "Right Angled";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkRightAngledC() {
+        System.out.println("Checking Right Angled C");
+        Triangle instance = new Triangle("5", "4", "3");
+        String expResult = "Right Angled";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkGiantTriangle() {
+        System.out.println("Checking Giant Triangle");
+        Triangle instance = new Triangle("3000000", "4000000", "3000000");
+        String expResult = "I feel your triangle is too big\n";
+        String result = instance.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkInvalidTriangleA() {
+        System.out.println("Checking Invalid Triangle A");
+        Triangle instance1 = new Triangle("3", "4", "8");
+        String expResult = "The lengths of the triangles do not form a valid triangle!\n";
+        String result = instance1.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkInvalidTriangleB() {
+        System.out.println("Checking Invalid Triangle B");
+        Triangle instance1 = new Triangle("3", "8", "4");
+        String expResult = "The lengths of the triangles do not form a valid triangle!\n";
+        String result = instance1.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void checkInvalidTriangleC() {
+        System.out.println("Checking Invalid Triangle C");
+        Triangle instance1 = new Triangle("8", "4", "3");
+        String expResult = "The lengths of the triangles do not form a valid triangle!\n";
+        String result = instance1.determineTriangleType();
+        assertEquals(expResult, result);
+    }
+}
